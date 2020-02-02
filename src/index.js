e788905019aea686dbf620d52aef5edd139b4e0b
hyerim1048 <gpflav@gmail.com>
2020-02-02 22:05:39 +0900

    update middleware - redux-logger to see logs

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { Provider, connect } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { searchRobots } from './reducers';
+import { createLogger } from 'redux-logger';
 
 // connect : a component which knows redux store
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger))
 // reducers 는 많아질 수 있음 
 
 
